010adbc3ec84f983a70696e5610cc6c11c9c56b1
AlexeyS <xengawe@gmail.com>
2020-12-08 14:19:54 -0800

    complete inheritence

--- W4D2/class_inheritance/inheirtance.rb
+++ W4D2/class_inheritance/inheirtance.rb
@@ -1,5 +1,7 @@
 class Employee
 
+  attr_reader :salary
+
   def initialize(name, title, salary, boss)
     @name = name
     @title = title
@@ -15,9 +17,9 @@ end
 
 class Manager < Employee
 
-  def initialize(name, title, salary, boss, employees)
+  def initialize(name, title, salary, boss)
     super(name, title, salary, boss)
-    @employees = employees 
+    @employees = []
   end
 
   def bonus(multiplier)
@@ -27,11 +29,23 @@ class Manager < Employee
     end
     total
   end
-end
 
+  def add_employee(*another_employee)
+    @employees += another_employee
+  end
+
+end
 
 ned = Manager.new("Ned", "Founder", 1000000, nil)
 darren = Manager.new("Darren", "TA Manager",78000, ned)
-shawna = Employee.new("Shawna", "TA", 12000, darren)
-david = Employee.new("David", "TA", 10000, darren)
 
+david = Employee.new("David", "TA", 10000, darren)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+
+darren.add_employee(david)
+darren.add_employee(shawna)
+ned.add_employee(darren, shawna, david)
+
+p ned.bonus(5)
+p darren.bonus(4)
+p david.bonus(3)
