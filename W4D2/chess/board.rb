af5f57f8c74653a242c53bc50d065c37e03b8ccc
AlexeyS <xengawe@gmail.com>
2020-12-08 15:24:59 -0800

    complete phase 1 chess

--- W4D2/chess/board.rb
+++ W4D2/chess/board.rb
@@ -1,6 +1,7 @@
+
 class Board
   def initialize()
-    @board = Array.new(8) {Array.new(8, nil)}
+    @board = Board.make_new_board
   end
 
   def [](pos)
@@ -19,6 +20,19 @@ class Board
     raise "Invalid move" if !self[end_pos].nil?
     self[end_pos] = self[start_pos]
     self[start_pos] = nil 
+  end
+
+  def self.make_new_board # return 2d array
+    board = Array.new(8) {Array.new(8, nil)}
+    board[0][0] = "Rook"
+    board[0][7] = "Rook"
+    board[7][0] = "Rook"
+    board[7][7] = "Rook"
+    board[0][1] = "Knight"
+    board[0][6] = "Knight"
+    board[7][1] = "Knight"
+    board[7][6] = "Knight"
+    board 
   end
   
 end
