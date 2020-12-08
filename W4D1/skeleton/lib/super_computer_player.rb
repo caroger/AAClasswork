78a33bda424b42fa2eec1296688dd5328c16b736
caroger <rogerhu1989@gmail.com>
2020-12-07 20:29:53 -0800

    complete W4D1 classwork

--- W4D1/skeleton/lib/super_computer_player.rb
+++ W4D1/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,18 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    possible_moves = node.children
+
+    node = possible_moves.find { |node| node.winning_node?(mark) }
+    return node.prev_move_pos if node
+
+    node = possible_moves.find { |node| !node.losing_node?(mark) }
+    return node.prev_move_pos if node
+
+    raise "computer is going to lose T.T"
+
   end
 end
 
