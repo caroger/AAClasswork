e0e9e9e411766443a43d93b06deac0cb33efc899
caroger <rogerhu1989@gmail.com>
2020-12-14 15:45:05 -0800

    ResizingIntSet

--- W5D1/MyHashMap/lib/p01_int_set.rb
+++ W5D1/MyHashMap/lib/p01_int_set.rb
@@ -42,7 +42,7 @@ class IntSet
   end
 
   def insert(num)
-    raise 'num already in set' if include?(num)
+    return if include?(num)
 
     self[num] << num
   end
@@ -76,18 +76,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return if include?(num)
+
+    resize! if @count >= num_buckets
+
+    @count += 1
+    self[num] << num
   end
 
   def remove(num)
+    return unless include?(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -95,5 +106,11 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.flatten.each do |ele|
+      insert(ele)
+    end
   end
 end
