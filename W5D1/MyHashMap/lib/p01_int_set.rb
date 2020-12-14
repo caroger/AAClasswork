c57a66ba0ebaddffccadc430d9eb5d3068875f8f
caroger <rogerhu1989@gmail.com>
2020-12-14 15:01:25 -0800

    complete intSet

--- W5D1/MyHashMap/lib/p01_int_set.rb
+++ W5D1/MyHashMap/lib/p01_int_set.rb
@@ -36,23 +36,30 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) {[]}
   end
 
   def insert(num)
+    raise 'num already in set' if include?(num)
+
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
