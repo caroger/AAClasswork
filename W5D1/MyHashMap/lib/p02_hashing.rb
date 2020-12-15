fa1f56dd6248e5ce0df66abc83c2118f1040de58
caroger <rogerhu1989@gmail.com>
2020-12-14 16:17:36 -0800

    hasing

--- W5D1/MyHashMap/lib/p02_hashing.rb
+++ W5D1/MyHashMap/lib/p02_hashing.rb
@@ -4,18 +4,20 @@ end
 
 class Array
   def hash
+    self.each_with_index.inject(0) do |accu, (ele, idx)|
+      (ele.hash + idx.hash) ^ accu
+    end
   end
 end
 
 class String
   def hash
+    self.chars.map { |char| char.ord }.hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.hash
   end
 end
