4cffc2d35634f8ca808b42821dc191ecf702058f
caroger <rogerhu1989@gmail.com>
2020-12-14 16:59:29 -0800

    hash_set

--- W5D1/MyHashMap/lib/p03_hash_set.rb
+++ W5D1/MyHashMap/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,26 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+
+    resize! if @count >= num_buckets
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    (self[key].delete(key); @count -= 1) if include?(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash  % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +34,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.flatten.each do |ele|
+      insert(ele)
+    end
   end
 end
