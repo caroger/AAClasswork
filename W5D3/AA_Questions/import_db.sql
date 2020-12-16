b6d0eb9ba3bcb145123088069f4da5c95bcff508
SediqaFahimi <sediqa.fahimi@gmail.com>
2020-12-16 14:20:57 -0800

    Worked on creating tables

--- W5D3/AA_Questions/import_db.sql
+++ W5D3/AA_Questions/import_db.sql
@@ -54,13 +54,35 @@ CREATE TABLE question_likes(
 INSERT INTO 
     users(f_name,l_name) /*specifies the order we will be supplying values in below*/
 VALUES 
-    ('Fred','Tabby');
+    ('Fred','Tabby'),
+    ('Sediqa','Fahimi'),
+    ('Roger','Hu');
 
--- CREATE TABLE cattoys(
---     id INTEGER PRIMARY KEY,
---     cat_id INTEGER,
---     toy_id INTEGER,
+INSERT INTO 
+    questions(title,body) /*specifies the order we will be supplying values in below*/
+VALUES 
+    ('SQL','How to create a table?'),
+    ('Ruby','How to bubble sort?'),
+    ('HTML','How to render HTML?');
 
---     FOREIGN KEY (cat_id) REFERENCES cats(id),
---     FOREIGN KEY (toy_id) REFERENCES toys(id)
--- )
+INSERT INTO 
+    questions_follows(question_id,user_id) /*specifies the order we will be supplying values in below*/
+VALUES 
+    (1,3),
+    (2,1),
+    (2,2);
+
+INSERT INTO 
+    replies(body,parent_id,user_id,question_id) /*specifies the order we will be supplying values in below*/
+VALUES 
+    ('Reply to ruby question by Sediqa',NULL, 2, 2),
+    ('Reply to HTML question by Roger', NULL, 3, 3),
+    ('Reply to Rogers answer on HTML by Sediqa', 2, 2, 3);
+
+INSERT INTO 
+    question_likes(user_id, question_id) /*specifies the order we will be supplying values in below*/
+VALUES 
+    (1 , 3),
+    (1 , 2),
+    (3 , 2);
+
