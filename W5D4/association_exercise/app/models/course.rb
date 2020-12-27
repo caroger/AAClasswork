79d240ebb910f4e20cde4edb2c52265acfe20fda
caroger <rogerhu1989@gmail.com>
2020-12-26 17:02:56 -0800

    finished W5D4 class proj

--- W5D4/association_exercise/app/models/course.rb
+++ W5D4/association_exercise/app/models/course.rb
@@ -10,4 +10,24 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :student
+
+  has_many :prerequisite,
+    class_name: :Course,
+    primary_key: :id,
+    foreign_key: :prereq_id
+
+  belongs_to :prerequisite,
+    class_name: :Course
+
+  belongs_to :instructor,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :instructor_id
 end
--- W5D4/association_exercise/app/models/user.rb
+++ W5D4/association_exercise/app/models/user.rb
@@ -8,10 +8,12 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-  has_many :enrollments
-    class_name: :Enrollment
-    primary_key: :id
+  has_many :enrollments,
+    class_name: :Enrollment,
+    primary_key: :id,
     foreign_key: :student_id
-    
-    
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
