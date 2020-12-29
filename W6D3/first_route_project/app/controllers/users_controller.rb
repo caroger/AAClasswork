bfc46bf5a75dfe57dfe0a97bda1ae28b9c3d0e35
shinara03 <shinara03@gmail.com>
2020-12-29 15:40:25 -0800

    Finish first route project

--- W6D3/first_route_project/app/controllers/users_controller.rb
+++ W6D3/first_route_project/app/controllers/users_controller.rb
@@ -1,13 +1,49 @@
 class UsersController < ApplicationController
   def index 
-    render plain: "I'm in the index action!"
+    # render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    render json: params
+    # render json: params
+
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
+    # user.save!
+    # render json: user
+    debugger
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+
   end
 
   def show
-    render json: params
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      # redirect_to "/users/#{user.id}"
+      # redirect_to user_url(user.id)
+      redirect_to user_url(user)
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+
+    user.destroy
+    render json: user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- W6D3/first_route_project/db/schema.rb
+++ W6D3/first_route_project/db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2020_12_29_223935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tests", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
