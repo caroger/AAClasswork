b52be9145720981bd66ccdc6566a9da6fe21d4bf
caroger <rogerhu1989@gmail.com>
2021-01-01 17:37:27 -0800

    ArtShareAPI - Phase 1

--- W6D1/arts_share_api/app/controllers/users_controller.rb
+++ W6D1/arts_share_api/app/controllers/users_controller.rb
@@ -3,12 +3,39 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
-
+  
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    render json: params.find(params[:id])
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: user
+   
+    
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
