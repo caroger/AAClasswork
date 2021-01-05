0d5956acdf8c89e105a67dee99472766e65709e2
droid4alex <droid4alex@gmail.com>
2021-01-04 16:37:45 -0800

    Finished PhaseIV

--- W7D1/Cats_Auth_2/app/controllers/sessions_controller.rb
+++ W7D1/Cats_Auth_2/app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+# In the User and Session controllers, use a before_action callback to redirect the user to the cats index if the user tries to visit the login/signup pages when they're already signed in.
 
   def new
     @user = User.new
@@ -6,17 +7,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #debugger #remember to use debugger to solve bugs!!!
     @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
 
     if @user
       login(@user)
-      redirect_to user_url(@user)
+      redirect_to cats_url
     else
       render :new
     end
   end
 
-  def destory
+  def destroy
     logout
     redirect_to new_session_url
   end
