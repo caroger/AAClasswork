f8bf91dcd3ed8cc4839ee9fd9e743dec06ef17e9
caroger <rogerhu1989@gmail.com>
2021-01-04 17:56:17 -0800

    finsihed phase 4

--- W7D1/Cats_Auth_2/app/models/user.rb
+++ W7D1/Cats_Auth_2/app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6}, allow_nil: true
   after_initialize :ensure_session_token
 
+  has_many :cats,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class: :Cat
+
+
   attr_reader :password
 
   def ensure_session_token
