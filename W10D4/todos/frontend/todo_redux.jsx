eece9ed1561ef7c76f11e08a5c44ef05ae7159b1
caroger <rogerhu1989@gmail.com>
2021-01-28 11:58:53 -0800

    todos redux project setup

---
+++ W10D4/todos/frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(
+    <h1>Welcome to my to-do app</h1>,
+    document.getElementById("root")
+  );
+});
