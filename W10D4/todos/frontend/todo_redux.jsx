6acc8c653175a77df95e2e9b9ee7074fc6e6065c
caroger <rogerhu1989@gmail.com>
2021-01-28 17:01:10 -0800

    started on remove todo

--- W10D4/todos/frontend/todo_redux.jsx
+++ W10D4/todos/frontend/todo_redux.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "../frontend/store/store";
+import { receiveTodo, receiveTodos } from "./actions/todo_actions";
 
 document.addEventListener("DOMContentLoaded", function () {
+  const store = configureStore;
+  window.store = store;
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
+
   ReactDOM.render(
     <h1>Welcome to my to-do app</h1>,
     document.getElementById("root")
