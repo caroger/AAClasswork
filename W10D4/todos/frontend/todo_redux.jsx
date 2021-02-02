599986015ccb9a63ec92c6281b69afef992da127
caroger <rogerhu1989@gmail.com>
2021-02-01 16:19:34 -0800

    part 2 phase 0

--- W10D4/todos/frontend/todo_redux.jsx
+++ W10D4/todos/frontend/todo_redux.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/app";
 import configureStore from "../frontend/store/store";
 import { receiveTodo, receiveTodos, removeTodo } from "./actions/todo_actions";
 import { receiveStep, receiveSteps, removeStep } from "./actions/step_actions";
+// const store = configureStore;
+// window.store = store;
+// window.receiveTodo = receiveTodo;
+// window.receiveTodos = receiveTodos;
+// window.removeTodo = removeTodo;
+// window.receiveStep = receiveStep;
+// window.receiveSteps = receiveSteps;
+// window.removeStep = removeStep;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const store = configureStore;
-  window.store = store;
-  window.receiveTodo = receiveTodo;
-  window.receiveTodos = receiveTodos;
-  window.removeTodo = removeTodo;
-  window.receiveStep = receiveStep;
-  window.receiveSteps = receiveSteps;
-  window.removeStep = removeStep;
-
-  ReactDOM.render(
-    <h1>Welcome to my to-do app</h1>,
-    document.getElementById("root")
-  );
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("content");
+  ReactDOM.render(<App />, root);
 });
