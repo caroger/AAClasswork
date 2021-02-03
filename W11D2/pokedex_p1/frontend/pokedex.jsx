7015b5a549b706be07ede5d5d448c66bda07ec33
caroger <rogerhu1989@gmail.com>
2021-02-03 14:26:24 -0800

    feat: APIUtil#fetchAllPokemon

--- W11D2/pokedex_p1/frontend/pokedex.jsx
+++ W11D2/pokedex_p1/frontend/pokedex.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {receiveAllPokemon } from "./actions/pokemon_actions"
+import {fetchAllPokemon} from "./util/api_util"
+
+// TODO: testing
+window.receiveAllPokemon = receiveAllPokemon;
+window.fetchAllPokemon = fetchAllPokemon
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("root");
