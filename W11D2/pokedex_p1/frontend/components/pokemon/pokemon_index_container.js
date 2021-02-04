72fecb0ef363e1d4e3c8d49b5cca7ae170e674b9
caroger <rogerhu1989@gmail.com>
2021-02-03 23:38:59 -0800

    started phase 3

---
+++ W11D2/pokedex_p1/frontend/components/pokemon/pokemon_index_container.js
@@ -0,0 +1,6 @@
+import { connect } from "react-redux";
+import { selectAllPokemon } from "../../reducers/selectors";
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
