2fa979fadb8606fc066dbe86ea4e2abe1b29a05d
caroger <rogerhu1989@gmail.com>
2021-02-03 18:47:25 -0800

    feat: store

--- W11D2/pokedex_p1/frontend/pokedex.jsx
+++ W11D2/pokedex_p1/frontend/pokedex.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {receiveAllPokemon } from "./actions/pokemon_actions"
-import {fetchAllPokemon} from "./util/api_util"
+import configureStore from "../frontend/store/store";
+import { receiveAllPokemon } from "./actions/pokemon_actions";
+import { fetchAllPokemon } from "./util/api_util";
 
-// TODO: testing
+// TODO: for testing only
 window.receiveAllPokemon = receiveAllPokemon;
-window.fetchAllPokemon = fetchAllPokemon
+window.fetchAllPokemon = fetchAllPokemon;
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById("root");
+  let store;
+  store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
--- W11D2/pokedex_p1/frontend/reducers/pokemon_reducer.js
+++ W11D2/pokedex_p1/frontend/reducers/pokemon_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_ALL_POKEMON } from "../actions/pokemon_actions";
 
 const pokemonReducer = (state = {}, action) => {
-  object.freeze(state);
+  Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ALL_POKEMON:
       return Object.assign({}, action.pokemon, state);
