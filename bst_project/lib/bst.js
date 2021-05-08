871334385dd5929872e474071e3cdeb3ba00e53f
caroger <rogerhu1989@gmail.com>
2021-05-08 15:55:04 -0700

    aa bst

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -1,17 +1,73 @@
 class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-
 class BST {
-   
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, root = this.root) {
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val < root.val) {
+      if (!root.left) {
+        root.left = new TreeNode(val);
+      } else {
+        this.insert(val, root.left);
+      }
+    } else {
+      if (!root.right) {
+        root.right = new TreeNode(val);
+      } else {
+        this.insert(val, root.right);
+      }
+    }
+  }
+
+  searchRecur(val, root = this.root) {
+    if (!root) return false;
+
+    if (val > root.val) {
+      return this.searchRecur(val, root.right);
+    } else if (val < root.val) {
+      return this.searchRecur(val, root.left);
+    } else if (val === root.val) return true;
+
+    // return false;
+  }
+
+  searchIter(val) {
+    if (!this.root) return false;
+
+    let cur = this.root;
+    while (cur !== null) {
+      if (cur.val === val) return true;
+      if (val < cur.val) cur = cur.left;
+      else if (val > cur.val) cur = cur.right;
+    }
+    return false;
+  }
+
+  inOrderPrint(root = this.root) {
+    if (!root) return;
+    this.inOrderPrint(root.left);
+    console.log(root.val);
+    this.inOrderPrint(root.right);
+  }
 }
 
 module.exports = {
-    TreeNode,
-    BST
+  TreeNode,
+  BST,
 };
+
+let tree = new BST();
+
