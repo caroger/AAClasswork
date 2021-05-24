0d3aedd4e2c8f4be8b48afe5d9567f8df6b0a065
Roger Hu <rogerhu1989@gmail.com>
2021-05-23 21:47:48 -0700

    concise code

--- heap_sort.py
+++ heap_sort.py
@@ -6,24 +6,13 @@ def heapify(arr, n, i):
     v = arr[i]
     left_i = i * 2 + 1
     right_i = i * 2 + 2
-    left_v = right_v = swap_i = None
 
-    if left_i >= n:
-        left_v = float("-inf")
-    else:
-        left_v = arr[left_i]
-    if right_i >= n:
-        right_v = float("-inf")
-    else:
-        right_v = arr[right_i]
+    left_v = float("-inf") if left_i >= n else arr[left_i]
+    right_v = float("-inf") if right_i >= n else arr[right_i]
 
     if v > left_v and v > right_v:
         return
     swap_i = left_i if left_v > right_v else right_i
-    # if left_v > right_v:
-    #     swap_i = left_i
-    # else:
-    #     swap_i = right_i
 
     swap(arr, i, swap_i)
     heapify(arr, n, swap_i)
